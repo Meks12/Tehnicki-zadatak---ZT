614c4ffa36dfd1b16a802849aea7516fb0d27c49
Meks <nikola.merlic2@gmail.com>
2025-05-16 17:16:02 +0200

    Učitavanje datoteke putem argumenta i provjera uvjeta (otvaranja)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
 
-int main(){
-    std::cout << "Pozdrav iz prvog Cmake C++ projekta" << std::endl;
+int main(int argc, char* argv[]) {
+    // Ovdje provjeravam ukoliko je korisnik unio točno jedan argument
+    if (argc !=2)    {
+        std::cerr <<" Upotreba: " << argv[0] << " <putanja_do_datoteke>" << std::endl;
+        return 1;
+    }
+    // Dohvacanje putanje iz komandne linije 
+    std::string putanja = argv[1];
+
+    // Otvaranje datoteke
+    std::ifstream datoteka(putanja);
+
+    // Uvjet koji provjerava da li je datoteka uspješno otvorena
+    if (!datoteka.is_open()){
+        std::cerr << "Greska ne mogu otvoriti datoteku!" << putanja << std::endl;
+        return 1;
+    }
+
+    std::cout << "Datoteka uspješno otvorena!" << putanja << std::endl;
+    datoteka.close();
+
     return 0;
+
 }
